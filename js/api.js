46e6559e9c27bbde961843153a33873fe25a5d8c
Vlad Herasymenko <drimacus182@gmail.com>
2018-12-04 15:51:52 +0200

    https зашьол

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 var api = (function(){
     var module = {};
 
-    var API_HOST = 'http://api-x32.texty.org.ua/';
+    var API_HOST = 'https://api-x32.texty.org.ua/';
 
     module.submitAndGetUsers = function(name, score, cb) {
         var json_str = encodeURI(JSON.stringify({name: name, score: score}));
